925928c95026ebe026014100878d504dc8792c90
Abhiram Dapke <47953521+abhiramdapke@users.noreply.github.com>
2020-02-22 12:06:27 -0500

    Create Single and Multi-level Inheritance.cpp

---
+++ Single and Multi-level Inheritance.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+// Single Inheritance
+class Vehicle
+{
+public:
+    void drivevehicle() {cout << "Driving" << endl;}
+};
+
+class Car : public Vehicle
+{
+public:
+    void gettires() {cout << "4 tires" << endl;}
+};
+
+
+//Multilevel Inheritance(Extemded from single level Inheritance)
+class Bmw
+{
+public:
+    void style() {cout << "Style" << endl;}
+};
+
+class Lamborghini
+{
+public:
+    void cost() {cout << "cost" << endl;}
+};
+
+int main()
+{
+    Vehicle v;
+    Car c;
+    Bmw b;
+    Lamborghini l;
+    
+    c.gettires();
+    c.drivevehicle();
+    
+    b.style();
+    l.cost();
+
+    return 0;
+}
