cdd3dbeb4c8a0d41d2f1485016bfc78a2ec7456c
Abhiram Dapke <47953521+abhiramdapke@users.noreply.github.com>
2020-03-23 21:10:43 -0400

    Create Find the length of a string.cpp

---
+++ Find the length of a string.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char str[] = "Welcome";
+    int i;
+    for (i=0;str[i]!='\0';i++)
+    {}
+    cout<< "Length of a string is " << i << endl;
+
+    return 0;
+}
