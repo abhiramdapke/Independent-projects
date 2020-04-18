d6e4d8b73dd1ca39f8470a6813c84fd3a7472507
Abhiram Dapke <47953521+abhiramdapke@users.noreply.github.com>
2020-04-18 11:19:36 -0400

    Update Static data members and static member functions example.cpp

--- Static data members and static member functions example.cpp
+++ Static data members and static member functions example.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+//We use static member functions only to work with static data members.
+/*
+1. Static data members are class members that are declared using the static keyword.
+2. There is only one copy of the static data member in the class, even if there are many class objects. 
+3. This is because all the objects share the static data member.
+4. The static data member is always initialized to zero when the first class object is created.
+
+
+1. A static member function is a special member function, which is used to access only static data members,
+ any other normal data member cannot be accessed through static member function.
+2. Just like static data member, static member function is also a class function;
+ it is not associated with any class object.
+*/
+
 class Point
 {
     int x,y;
