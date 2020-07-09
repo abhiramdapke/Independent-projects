e309d8d1a2027439b9be42c522fd9e2fe18c47e2
Abhiram Dapke <47953521+abhiramdapke@users.noreply.github.com>
2020-07-09 19:01:42 -0400

    Create Find sum of digits of a number.cpp

---
+++ Find sum of digits of a number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,r,sum=0;
+    cout << "Enter a number" << endl;
+    cin >> n;
+    while (n>0)
+    {
+        r = n%10;
+        sum = sum + r;
+        n = n/10;
+    }
+    cout << "Sum of digits of a number is " << sum << endl;
+
+    return 0;
+}
