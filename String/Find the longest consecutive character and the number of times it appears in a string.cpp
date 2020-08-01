cbaf78a0825eab3d876cb8eb313e3323f112996a
Abhiram Dapke <47953521+abhiramdapke@users.noreply.github.com>
2020-08-01 16:38:01 -0400

    Update Find the longest consecutive character and the number of times it appears in a string.cpp

--- String/Find the longest consecutive character and the number of times it appears in a string.cpp
+++ String/Find the longest consecutive character and the number of times it appears in a string.cpp
@@ -3,6 +3,7 @@
 #include <utility>
 using namespace std;
 
+//Method 1 - using pairs
 pair<char, int> longestconsecutivechar(string s, int &n)
 {
     int max_count = 0;
@@ -35,3 +36,44 @@ int main()
    
     return 0;
 }
+
+//Method 2 - TC - O(n)
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Time complexity - O(n)
+
+char longestconsecutivechar(string s, int &n)
+{
+    int curr_count = 1;
+    int count = 0;
+    char res = s[0];
+    for (int i=0;i<n;i++)
+    {
+        if (s[i] == s[i+1] && i+1<n )
+        {
+            curr_count++;
+        }
+        else
+        {
+            if (curr_count>count)
+            {
+                count = curr_count;
+                res = s[i];
+            }
+            curr_count = 1;
+        }
+    }
+    return res;
+}
+
+int main()
+{
+   string s = "AABBHHDDSSNNAUEJSHFbbbbbbBSSSS";
+   int n = s.size();
+   cout << longestconsecutivechar(s, n);
+   
+   return 0;
+}
+
