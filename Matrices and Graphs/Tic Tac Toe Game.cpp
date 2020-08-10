eee02789d7bae69fae39b54b7271cbd528f1e07f
Abhiram Dapke <47953521+abhiramdapke@users.noreply.github.com>
2020-08-10 16:50:58 -0400

    Update Tic Tac Toe Game.cpp

--- Matrices and Graphs/Tic Tac Toe Game.cpp
+++ Matrices and Graphs/Tic Tac Toe Game.cpp
@@ -83,25 +83,25 @@ char win()
         
     //second player
     //rows
-    if (matrix[0][0] == 'x' && matrix[0][1] == 'x' && matrix[0][2] =='x')
+    if (matrix[0][0] == 'o' && matrix[0][1] == 'o' && matrix[0][2] =='o')
         return 'o';
-    if (matrix[1][0] == 'x' && matrix[1][1] == 'x' && matrix[1][2] =='x')
+    if (matrix[1][0] == 'o' && matrix[1][1] == 'o' && matrix[1][2] =='o')
         return 'o';
-    if (matrix[2][0] == 'x' && matrix[2][1] == 'x' && matrix[2][2] =='x')
+    if (matrix[2][0] == 'o' && matrix[2][1] == 'o' && matrix[2][2] =='o')
         return 'o';
         
     //columns    
-    if (matrix[0][0] == 'x' && matrix[1][0] == 'x' && matrix[2][0] =='x')
+    if (matrix[0][0] == 'o' && matrix[1][0] == 'o' && matrix[2][0] =='o')
         return 'o';
-    if (matrix[0][1] == 'x' && matrix[1][1] == 'x' && matrix[2][1] =='x')
+    if (matrix[0][1] == 'o' && matrix[1][1] == 'o' && matrix[2][1] =='o')
         return 'o';
-    if (matrix[0][2] == 'x' && matrix[1][2] == 'x' && matrix[2][2] =='x')
+    if (matrix[0][2] == 'o' && matrix[1][2] == 'o' && matrix[2][2] =='o')
         return 'o';
         
     //diagonals 
-    if (matrix[0][0] == 'x' && matrix[1][1] == 'x' && matrix[2][2] =='x')
+    if (matrix[0][0] == 'o' && matrix[1][1] == 'o' && matrix[2][2] =='o')
         return 'o';
-    if (matrix[0][2] == 'x' && matrix[1][1] == 'x' && matrix[2][0] =='x')
+    if (matrix[0][2] == 'o' && matrix[1][1] == 'o' && matrix[2][0] =='o')
         return 'o';
     
     return '/';   
